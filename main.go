05e552ca464101064486acdb1f01828e9af875b0
Grace <g010329@gmail.com>
2021-12-19 21:37:24 +0800

    Add function of crawling jobName, CustName, skillSet

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 
 	"gopkg.in/resty.v1"
 )
@@ -13,6 +15,7 @@ type Job struct {
 
 type DataWrapper struct {
 	JobDetail *JobDetailWrapper `json:"jobDetail"`
+	Header    *HeaderWrapper    `json:"header"`
 }
 
 type JobDetailWrapper struct {
@@ -22,25 +25,64 @@ type JobDetailWrapper struct {
 	SalaryMax      int    `json:"salaryMax"`
 }
 
+type HeaderWrapper struct {
+	JobName  string `json:"jobName"`
+	CustName string `json:"custName"`
+}
+
+func checkSubstrings(str string, subs ...string) (bool, int, []string) {
+
+	matches := 0
+	matchWord := []string{}
+	isCompleteMatch := true
+
+	// fmt.Printf("String: \"%s\", Substrings: %s\n", str, subs)
+
+	for _, sub := range subs {
+		if strings.Contains(str, sub) {
+			matchWord = append(matchWord, sub)
+			matches += 1
+		} else {
+			isCompleteMatch = false
+		}
+	}
+
+	return isCompleteMatch, matches, matchWord
+}
+
 func main() {
-	f, err := os.Create("test.csv") //创建文件
+
+	f, err := os.Create("example.csv")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	var test *Job
+	var data *Job
 	client := resty.New()
-	resp, err := client.R().
+	client.R().
 		SetHeader("Referer", "https://www.104.com.tw/job/ajax/content/6hr41").
-		SetResult(&test).
+		SetResult(&data).
 		Get("https://www.104.com.tw/job/ajax/content/6hr41")
 
-	if err != nil {
-		fmt.Println("  Error      :", err)
+	// if err != nil {
+	// 	fmt.Println("  Error      :", err)
+	// }
+	// fmt.Println("  Body       :\n", resp)
+	// fmt.Println("  data       :\n", data.Data.Header.JobName)
+
+	isCompleteMatch1, matches1, test := checkSubstrings(data.Data.JobDetail.JobDescription, "react", "redux", "ES6", "typescript", "UI/UX")
+	fmt.Printf("Test 1: { isCompleteMatch: %t, Matches: %d }\n", isCompleteMatch1, matches1)
+	fmt.Println(test)
+
+	f.WriteString("\xEF\xBB\xBF")
+	w := csv.NewWriter(f)
+	csvData := [][]string{
+		{data.Data.Header.JobName, data.Data.Header.CustName, data.Data.JobDetail.Salary, strings.Join(test, ", ")},
 	}
-	fmt.Println("  Body       :\n", resp)
-	fmt.Println("  test       :\n", test.Data.JobDetail.JobDescription)
+	w.WriteAll(csvData)
+	w.Flush()
+
 }
 
 // resty
@@ -54,3 +96,7 @@ func main() {
 
 // Skill Set:
 // ["react","redux","ES6"]
+
+// data studio
+
+// 搜尋入口->搜尋結果的api url->爬取所有資料->比對skill set->上lambda-->經過處理，大家各自的table變成大table
