c3a846b76d47ef957231f029589b580bb4277bd3
Grace <g010329@gmail.com>
2021-12-27 22:43:28 +0800

    Create csv result of searching keyword

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # crawler
+
+<!-- https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/30332/ -->
+
+
+reference：
+https://ithelp.ithome.com.tw/questions/10198403
+https://blog.jiatool.com/posts/job104_spider/
+https://www.royenotes.com/python-104-employment-agency/
+
+curl --location --request GET 'https://www.104.com.tw/job/6hr41?jobsource=cs_2018indexpoc' \
+--header 'Accept-Language: en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7' \
+--header 'Referer: https://www.104.com.tw/job/6hr41' \
+--header 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36' \
+
+<!-- Notes -->
+技能會被放在： 工作內容、擅長工具、其他條件 裡面，要再修改
--- main.go
+++ main.go
@@ -3,32 +3,62 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+
+	// "fmt"
 	"os"
 	"strings"
 
 	"gopkg.in/resty.v1"
 )
 
-type Job struct {
+// 搜尋工作關鍵字結果list
+
+type SearchResult struct {
 	Data *DataWrapper `json:"data"`
 }
 
 type DataWrapper struct {
-	JobDetail *JobDetailWrapper `json:"jobDetail"`
-	Header    *HeaderWrapper    `json:"header"`
+	List      []*JobRow `json:"list"`
+	TotalPage int       `json:"totalPage"`
+	PageNo    int       `json:"pageNo"`
 }
 
-type JobDetailWrapper struct {
-	JobDescription string `json:"jobDescription"`
-	Salary         string `json:"salary"`
-	SalaryMin      int    `json:"salaryMin"`
-	SalaryMax      int    `json:"salaryMax"`
+type JobRow struct {
+	JobName     string `json:"jobName"`
+	CustName    string `json:"custName"`
+	Description string `json:"description"`
+	SalaryDesc  string `json:"salaryDesc"`
+	SalaryLow   int    `json:"salaryLow"`
+	SalaryHigh  int    `json:"salaryHigh"`
+	// jobNameRaw
+	// custNameRaw
+	// remoteWorkType
+	// optionEdu
+	// jobAddrNoDesc
+	// periodDesc
 }
 
-type HeaderWrapper struct {
-	JobName  string `json:"jobName"`
-	CustName string `json:"custName"`
-}
+// 單一工作結果
+// type Job struct {
+// 	Data *DataWrapper `json:"data"`
+// }
+
+// type DataWrapper struct {
+// 	JobDetail *JobDetailWrapper `json:"jobDetail"`
+// 	Header    *HeaderWrapper    `json:"header"`
+// }
+
+// type JobDetailWrapper struct {
+// 	JobDescription string `json:"jobDescription"`
+// 	Salary         string `json:"salary"`
+// 	SalaryMin      int    `json:"salaryMin"`
+// 	SalaryMax      int    `json:"salaryMax"`
+// }
+
+// type HeaderWrapper struct {
+// 	JobName  string `json:"jobName"`
+// 	CustName string `json:"custName"`
+// }
 
 func checkSubstrings(str string, subs ...string) (bool, int, []string) {
 
@@ -52,34 +82,67 @@ func checkSubstrings(str string, subs ...string) (bool, int, []string) {
 
 func main() {
 
-	f, err := os.Create("example.csv")
+	f, err := os.Create("example2.csv")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	var data *Job
+	// var data *Job
+	// client := resty.New()
+	// client.R().
+	// 	SetHeader("Referer", "https://www.104.com.tw/job/ajax/content/6hr41").
+	// 	SetResult(&data).
+	// 	Get("https://www.104.com.tw/job/ajax/content/6hr41")
+
+	// isCompleteMatch1, matches1, test := checkSubstrings(data.Data.JobDetail.JobDescription, "react", "redux", "ES6", "typescript", "UI/UX")
+	// fmt.Printf("Test 1: { isCompleteMatch: %t, Matches: %d }\n", isCompleteMatch1, matches1)
+	// fmt.Println("命中skill set: ", test)
+
+	// f.WriteString("\xEF\xBB\xBF")
+	// w := csv.NewWriter(f)
+	// csvData := [][]string{
+	// 	{data.Data.Header.JobName, data.Data.Header.CustName, data.Data.JobDetail.Salary, strings.Join(test, ", ")},
+	// }
+	// w.WriteAll(csvData)
+	// w.Flush()
+
+	// ----------------------------------------------------------------
+	var data *SearchResult
+
 	client := resty.New()
 	client.R().
-		SetHeader("Referer", "https://www.104.com.tw/job/ajax/content/6hr41").
+		SetHeader("Referer", "https://www.104.com.tw/jobs/search/").
+		SetQueryParams(map[string]string{
+			"ro":            "0",
+			"kwop":          "7",
+			"keyword":       "前端",
+			"expansionType": "area,spec,com,job,wf,wktm",
+			"asc":           "0",
+			"mode":          "s", // s or l
+			"jobsource":     "2018indexpoc",
+			"page":          "0",
+		}).
 		SetResult(&data).
-		Get("https://www.104.com.tw/job/ajax/content/6hr41")
+		Get("https://www.104.com.tw/jobs/search/list")
 
-	// if err != nil {
-	// 	fmt.Println("  Error      :", err)
-	// }
-	// fmt.Println("  Body       :\n", resp)
-	// fmt.Println("  data       :\n", data.Data.Header.JobName)
+	// totalPageNumber := data.Data.TotalPage
 
-	isCompleteMatch1, matches1, test := checkSubstrings(data.Data.JobDetail.JobDescription, "react", "redux", "ES6", "typescript", "UI/UX")
-	fmt.Printf("Test 1: { isCompleteMatch: %t, Matches: %d }\n", isCompleteMatch1, matches1)
-	fmt.Println(test)
+	// fmt.Printf("TotalPage: %d \n", totalPageNumber)
 
 	f.WriteString("\xEF\xBB\xBF")
-	w := csv.NewWriter(f)
-	csvData := [][]string{
-		{data.Data.Header.JobName, data.Data.Header.CustName, data.Data.JobDetail.Salary, strings.Join(test, ", ")},
+	var csvData [][]string
+
+	for i := 0; i < len(data.Data.List); i++ {
+
+		isCompleteMatch1, matches1, matchWords := checkSubstrings(data.Data.List[i].Description, "react", "redux", "ES6", "typescript", "UI/UX", "CSS", "Vue.js", "Vue", "Javascript", "RWD", "Git")
+		fmt.Printf("Test 1: { isCompleteMatch: %t, Matches: %d, word: %s }\n", isCompleteMatch1, matches1, strings.Join(matchWords, ", "))
+
+		csvData = append(csvData, []string{data.Data.List[i].JobName, data.Data.List[i].CustName, data.Data.List[i].SalaryDesc, strings.Join(matchWords, ", ")})
+		// csvData = append(csvData, []string{data.Data.List[i].JobName, data.Data.List[i].CustName, data.Data.List[i].SalaryDesc})
 	}
+
+	w := csv.NewWriter(f)
 	w.WriteAll(csvData)
 	w.Flush()
 
@@ -100,3 +163,9 @@ func main() {
 // data studio
 
 // 搜尋入口->搜尋結果的api url->爬取所有資料->比對skill set->上lambda-->經過處理，大家各自的table變成大table
+
+// https://www.104.com.tw/jobs/search/list?ro=0&kwop:=7&keyword=前端&expansionType=area,spec,com,job,wf,wktm&order=1&asc=0&page=1&mode=s&jobsource=2018indexpoc
+// ro: 0(不限) / 1(全職)
+// keyword:"前端"
+// area:'6001001000', # 限定在台北的工作 不限不填
+// 'isnew':'30', # 只要最近一個月有更新的過的職缺
